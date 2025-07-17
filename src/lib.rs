a958b6082884a0ec36baaaed652fc5c388e474fd
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-07-17 08:33:01 +0200

    test(directory reading): add doctest that runs file discovery function with temporary files.

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ use std::fs;
 /// # Arguments
 ///
 /// * `p` - A reference to a [`Path`] representing the root directory to traverse.
-/// * `f` - A reference to a closure that takes a reference to a [`Path`] and returns `()`
+/// * `f` - A mutable reference to a closure that takes a reference to a [`Path`] and returns `()`
 ///         (i.e., performs side effects such as logging, collecting paths, etc.).
 ///
 /// # Returns
@@ -24,17 +24,33 @@ use std::fs;
 /// Returns an [`io::ErrorKind::NotADirectory`] error if `p` is not a directory.
 /// Also propagates any errors returned by [`fs::read_dir`] or while accessing entries.
 ///
-/// # Examples
-///
+/// # Example
 /// ```
-/// use std::path::Path;
-/// use std::io;
-///
-/// fn main() -> io::Result<()> {
-///     static_preprocessing::for_each_file(Path::new("./some_dir"), &|path| {
-///         println!("Found file: {}", path.display());
-///     })
-/// }
+/// 
+///  # use std::fs;
+///  # use std::path::PathBuf;
+///  # use tempfile::TempDir;
+///  # use static_preprocessing::for_each_file;
+///  # /// Create temporary directory
+///  # let temp_dir = TempDir::new().unwrap();
+///  # let file0 = temp_dir.path().join("file0.txt");
+///  # let sub_dir = temp_dir.path().join("sub");
+///  # let sub_file = sub_dir.join("file1.jpeg");
+/// 
+///  # /// Create files
+///  # fs::File::create(&file0).unwrap();
+///  # fs::create_dir(&sub_dir).unwrap();
+///  # fs::File::create(&sub_file).unwrap();
+/// 
+///  /// Capture visited paths
+///  let mut visited: Vec<PathBuf> = Vec::new();
+/// 
+///  for_each_file(temp_dir.path(), &mut |p| visited.push(p.to_path_buf())).unwrap();
+/// 
+///  /// Check all files were visited
+///  assert!(visited.contains(&file0));
+///  assert!(visited.contains(&sub_file));
+///  assert_eq!(visited.len(), 2);
 /// ```
 ///
 /// [`Path`]: std::path::Path
@@ -42,7 +58,7 @@ use std::fs;
 /// [`Err`]: std::result::Result::Err
 /// [`fs::read_dir`]: std::fs::read_dir
 /// [`io::ErrorKind::NotADirectory`]: std::io::ErrorKind::NotADirectory
-pub fn for_each_file<F: Fn(&Path)>(p: &Path, f: &F) -> io::Result<()> {
+pub fn for_each_file<F: FnMut(&Path)>(p: &Path, f: &mut F) -> io::Result<()> {
     if p.is_dir() {
         for entry in fs::read_dir(p)? {
             let path = entry?.path();
